b658929a15c428c6da2582111fdd1881621af675
liamjcarlin <68574843+liamjcarlin@users.noreply.github.com>
2024-03-31 21:08:47 +1000

    Soder Day #007
    Added some velocity to the Keyframes. Essentially just the cubic from flow. Usually what I use!

--- LC_Trim Paths to Shape Layer.jsx
+++ LC_Trim Paths to Shape Layer.jsx
@@ -23,19 +23,24 @@ function addTrimPath() {
 app.endUndoGroup();
 
   }
-
+  
 function tp(layer) {
 
 for (var i = 0; i < layer.length; i++) {
 
+//Trim Path Property
 var effectsProperty = layer[i].property("ADBE Root Vectors Group");
 var trimPath = effectsProperty.addProperty("ADBE Vector Filter - Trim");
 var trimPathEnd = trimPath.property("ADBE Vector Trim End");
-
+//Trim Path Keyframes
+var applyEaseIn = new KeyframeEase(0,66);
+var applyEaseOut = new KeyframeEase(0,66);
 trimPathEnd.addKey(0);
 trimPathEnd.addKey(3);
 trimPathEnd.setValueAtKey(1,0);
 trimPathEnd.setValueAtKey(2,100);
+trimPathEnd.setTemporalEaseAtKey(1,[applyEaseIn],[applyEaseOut]);
+trimPathEnd.setTemporalEaseAtKey(2,[applyEaseIn],[applyEaseOut]);
 
 }
 
